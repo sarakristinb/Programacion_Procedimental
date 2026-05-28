387dc34350c7b14fe63caab7e3de717366ce8dbc
Sara Buitrago <sarakristinb@gmail.com>
2026-05-28 16:45:14 -0500

    se agregaron los datos de usuario

---
+++ Restaurante/js/contabilidad.js
@@ -0,0 +1,5 @@
+let ventas = document.getElementById("ventas").value;
+let ingresos = document.getElementById("ingresos").value;
+let egresos = document.getElementById("egresos").value;
+let concepto = document.getElementById("concepto").value;
+let valor = document.getElementById("valor").value;
---
+++ Restaurante/js/pedidos.js
@@ -0,0 +1,6 @@
+let nombreCliente = document.getElementById("nombre_cliente").value;
+let producto = document.getElementByName("producto").value;
+let cantidadProducto = document.getElementById("cantidad_producto").value;
+let direccionEntrega = document.getElementById("direccion_entrega").value;
+let telefonoCliente = document.getElementById("telefono_cliente").value;
+let metodoPago = document.querySelector('input[name="pago"]:checked').value;
