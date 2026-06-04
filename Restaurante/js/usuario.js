c1069eb016bef602dce5b30e1b792d912b0146a5
Sara Buitrago <sarakristinb@gmail.com>
2026-06-04 16:50:50 -0500

    Add validation to user form and require input fields

--- Restaurante/js/usuario.js
+++ Restaurante/js/usuario.js
@@ -8,5 +8,28 @@ let cargo_usuario = document.querySelector('select[name="cargo_usuario"]').value
 let fecha_nacimiento_usuario = document.getElementById("fecha_nacimiento_usuario").value;
 let contraseña_usuario = document.getElementById("contraseña_usuario").value;
 function ValidarDatos() {
-    // Aquí puedes agregar la lógica para enviar los datos al servidor o realizar otras acciones necesarias
-    console.log("Nombre de usuario:", nombre_usuario); }
+    if (nombre_usuario === "" || tipo_documento_usuario === "" || numero_documento_usuario === "" || telefono_usuario === "" || correo_usuario === "" || genero_usuario === "" || cargo_usuario === "" || fecha_nacimiento_usuario === "" || contraseña_usuario === "") {
+        alert("Por favor, completa todos los campos antes de enviar el formulario.");
+        return false; 
+    }
+    else {
+            if(nombre_usuario != /[a-zA-Z\s]+/.test(nombre_usuario)) {
+                console.log("los datos son inválidos.")
+            }
+            if (numero_documento_usuario.length < 10) {
+                console.log("los datos son inválidos."); 
+            }
+            if (telefono_usuario.length < 10) {
+                console.log("los datos son inválidos."); 
+            }
+            if (correo_usuario.includes("@") && correo_usuario.includes(".")) {
+                console.log("los datos son inválidos."); }
+            if (contraseña_usuario.length < 8 && contraseña_usuario.includes(".")) {
+                console.log("los datos son inválidos."); 
+            }
+                alert("¡Registro exitoso! Bienvenido al equipo.");
+        return true; // Permite que el formulario se envíe
+         
+
+    }
+}
