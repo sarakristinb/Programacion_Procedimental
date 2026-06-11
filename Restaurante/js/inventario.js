73aa7a84472b39e24473e00f092a522ffe45ab59
Sara Buitrago <sarakristinb@gmail.com>
2026-06-11 17:28:13 -0500

    validate_module_inventario : validate inputs

--- Restaurante/js/inventario.js
+++ Restaurante/js/inventario.js
@@ -2,3 +2,16 @@ let codigo = document.getElementById("codigo").value;
 let nombre = document.getElementById("nombre").value;
 let cantidad = document.getElementById("cantidad").value;
 let marca = document.getElementById("marca").value;
+
+if (typeof codigo === "number") {
+    console.log("Es un número inválido");
+}
+if (typeof nombre === "string") {
+    console.log("Es un nombre inválido");
+}   
+if (typeof cantidad === "number") {
+    console.log("Es un número inválido");
+}
+if (typeof marca === "string") {
+    console.log("Es una marca inválida");
+}
